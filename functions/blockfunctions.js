25264071f018df05d69688269908cf694472bba2
ardaasln <aslan.rda@gmail.com>
2017-05-06 23:21:10 +0300

    code reformat

--- functions/blockfunctions.js
+++ functions/blockfunctions.js
@@ -1,10 +1,11 @@
 /**
  * Created by ardaaslan on 04/05/2017.
  */
+var app = require('../app');
 
 var functions = {
     getBlockNumber: function (req, res) {
-        res.json({message: global.web3.eth.blockNumber});
+        res.json({message: app.web3.eth.blockNumber});
     }
 };
 
